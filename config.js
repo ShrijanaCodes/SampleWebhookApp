b82cba3634e5b34a7943f5736fb201dc3b2b34d7
Shrijana Ghimire <shrijanaghimire@Shrijanmire2928.attlocal.net>
2020-03-04 11:24:25 -0800

    added dotenv package to access the env variables from config file.

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
-const env = process.env.NODE_ENV || 'production'
-//Provide your credentials here: 
-const config = {
-	development :{
-		APIKey : '',
-		APISecret : ''
-	},
-	production:{	
-		APIKey : '',
-		APISecret : '',
-		VerificationToken: ''
-	}
-};
+// store the credential variables in .env file and retrieve the values here.
 
-module.exports = config[env]
+const dotenv = require('dotenv');
+dotenv.config();
+ module.exports= {
+  ZOOM_API_KEY: process.env.APIKey,
+  ZOOM_API_SECRET: process.env.APISecret,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  VERIFICATION_TOKEN: process.env.VerificationToken
+};
